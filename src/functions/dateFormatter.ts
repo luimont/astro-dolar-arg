80bf54aaa552a9f1770c4d65103b2d8fe5803b7b
Luis Montenegro <luismontenegro.1995@gmail.com>
2024-02-13 22:25:04 -0300

    Add formated Date

--- src/functions/dateFormatter.ts
+++ src/functions/dateFormatter.ts
@@ -2,7 +2,7 @@
 export const dateFormatter = (date: string) => {
 
   //const date = '2024-02-13T20:53:00.000Z';
-  const formattedDate = new Date(date).toLocaleString('es-AR');
+  const formattedDate = new Date(date).toLocaleString('es-AR', {timeZone:'America/Argentina/Buenos_Aires'});
 
   console.log("loglog"+formattedDate); // Ejemplo de salida: "13/02/2024, 16:53:00"
   return formattedDate
